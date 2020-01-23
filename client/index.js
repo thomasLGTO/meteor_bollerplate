ed5bc81f1f5db01f843eb070243787a536c27e2f
thomasLGTO <thomaslargeteau87@gmail.com>
2020-01-23 11:03:44 +0100

    premier affichage

--- client/index.js
+++ client/index.js
@@ -1,1 +1,8 @@
+import  React  from 'react'
+import  ReactDOM  from 'react-dom'
+import App from '/imports/routers/App'
 console.log('CLIENT : meteor est lancé')
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+)
---
+++ imports/routers/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App(props){
+    return(
+    <p>WHOUOUUU</p>
+    )
+}
+
+export default App
