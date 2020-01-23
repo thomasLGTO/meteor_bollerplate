cde42a56df4fcbfc90d19addf56227729750d0f2
thomasLGTO <thomaslargeteau87@gmail.com>
2020-01-23 12:47:35 +0100

    ajout d'un formulaire de connexion

--- imports/pages/Signup.js
+++ imports/pages/Signup.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Form, Button, Container} from 'semantic-ui-react'
 
 function Signup(props){
     return(
-    <p>Signup Page</p>
+    <Container>
+        <h1>Inscrivez-vous !</h1>
+        <Form size={"large"} action="">
+            <Form.Input label="Email" required  type="email"  placeholder="ex: toto@gmail.com"name="" id=""/>
+            <Form.Input label="Mot de passe" required type="password" name="" id=""/>
+            <Button color='blue'>m'inscrire</Button>
+        </Form>
+    </Container>
     )
 }
 
